2fa09a23a8df61ec38190df211004712ff0e5ec1
alvakleiner <alvakleiner@gmail.com>
2026-02-18 17:55:34 +0100

    fix: Update execute in WrapTextCommand so tests pass

--- src/main/java/edu/ntnu/idatt2003/textmanipulation/CapitalizeTextCommand.java
+++ src/main/java/edu/ntnu/idatt2003/textmanipulation/CapitalizeTextCommand.java
@@ -4,6 +4,9 @@ public class CapitalizeTextCommand implements TextCommand {
 
     @Override
     public String execute(String text) {
+        if (text == null || text.isEmpty()) {
+            return "";
+        }
         return text.substring(0, 1).toUpperCase() + text.substring(1);
     }
 }
